ea00c3e491586663105ca9a9b4081b80cec24ca3
Nazwa-Wijaya <nazwawijaya89@gmail.com>
2025-03-30 17:58:27 +0700

    push week2

--- quiz/part2/soal1.js
+++ quiz/part2/soal1.js
@@ -1,4 +1,6 @@
-//Diberikan sebuah function palindrome(kata) yang menerima satu parameter. Function tersebut akan mengembalikan nilai true jika kata merupakan palindrome, dan false jika bukan. Kata palindrome adalah sebuah kata yang jika dibalik, tetap sama. Contoh, 'katak' dibalik tetaplah 'katak'.
+//Diberikan sebuah function palindrome(kata) yang menerima satu parameter.
+//  Function tersebut akan mengembalikan nilai true jika kata merupakan palindrome, dan false jika bukan. 
+// Kata palindrome adalah sebuah kata yang jika dibalik, tetap sama. Contoh, 'katak' dibalik tetaplah 'katak'.
 
 
 function palindrome(kata) {
