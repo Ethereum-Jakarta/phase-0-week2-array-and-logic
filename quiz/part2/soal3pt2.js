b6680b67db8a2b715c099ea68249036d7278745a
MASTER8623 <160683659+MASTER8623@users.noreply.github.com>
2025-04-29 17:21:19 +0800

    submit perbaikan pada week2 dari bang harkon

--- quiz/part2/soal3pt2.js
+++ quiz/part2/soal3pt2.js
@@ -1,12 +1,32 @@
+/*
 function hitungJumlahKata(kalimat) {
     // you can only write your code here!
-    let uuperampasanasetkapandisahkanya = kalimat.split(' ').filter(uuperampasanasetkapandisahkanya => uuperampasanasetkapandisahkanya.length > 0);
-    return uuperampasanasetkapandisahkanya.length;
+    let uuperampasanasetkapandisahkanya = kalimat.split(' ').length
+    return uuperampasanasetkapandisahkanya
+}
+*/
+
+function hitungJumlahKata(kalimat) {
+    let total = 0
+    let word = false
+
+    
+    for (let i = 0; i < kalimat.length; i++) {
+      if(kalimat[i] !== ' ') {
+        if (!word){
+          total++;
+          word = true;
+        }
+      } else
+        word = false;
+    }
+    return total
   }
+    // TEST CASES
+    console.log(hitungJumlahKata('I have a dream')); // 4
+    console.log(hitungJumlahKata('Never eat shredded wheat or cake')); // 6
+    console.log(hitungJumlahKata('A song to sing')); // 4
+    console.log(hitungJumlahKata('I')); // 1
+    console.log(hitungJumlahKata('I believe I can code')); // 5
+
   
-  // TEST CASES
-  console.log(hitungJumlahKata('I have a dream')); // 4
-  console.log(hitungJumlahKata('Never eat shredded wheat or cake')); // 6
-  console.log(hitungJumlahKata('A song to sing')); // 4
-  console.log(hitungJumlahKata('I')); // 1
-  console.log(hitungJumlahKata('I believe I can code')); // 5
