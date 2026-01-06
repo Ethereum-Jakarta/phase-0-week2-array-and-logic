2fad2d3fe228a4ed4521df13f304f6ce3cf7cb3e
zegcamegon <zegcamegon@localhost.localdomain>
2026-01-07 06:21:20 +0700

    sudah selesai

---
+++ quiz/ujian/soal3.js
@@ -0,0 +1,27 @@
+/*
+diberikan sebuah function groupAnimals(animals) yang menerima satu parameter berupa array,
+fungsi ini akan me-return array 2 dimensi
+*/
+function groupAnimals(animals) {
+  // you can only write your code here!
+  let hasil = [];
+  let group = [];
+  animals.sort();
+  for (let i = 0; i < animals.length;i++) {
+    if (i === 0) {
+      group.push(animals[i]);
+    } else if (animals[i][0] === group[group.length - 1][0][0]) {
+      hasil[group.length -1].push(animals[i]);
+    } else {
+      group.push(animals[i]);
+    }
+      hasil.push(group);
+  }
+  return hasil;
+}
+
+// TEST CASES
+console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
+// [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
+console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
+// [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda', 'kancil'], ['unta'] ]
